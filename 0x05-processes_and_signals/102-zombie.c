f4d3423233d4148a847c5217ed2bcb4bf6030b96
cristiand187 <cristiand187@hotmail.com>
2020-06-23 03:06:57 +0000

    0x05. Processes and signals

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * infinite_while - sleep function
+ *
+ * Return: int value
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - function init
+ *
+ * Return: 0 - ok
+ */
+int main(void)
+{
+	int i = 0, id = 0;
+
+	for (i = 0; i < 5; i++)
+	{
+		id = fork();
+		if (id == 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(0);
+		}
+	}
+	infinite_while();
+	return (0);
+}
