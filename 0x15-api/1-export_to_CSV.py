f95b94314c56001f3968f3cb4f94a88eb35653bf
Cristiand187 <cristiand187@hotmail.com>
2020-09-14 19:45:05 -0500

    Task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,21 +2,19 @@
 """Write a Python script that, using this REST API, for a given
 employee ID, returns information about his/her TODO list progress."""
 import requests
-import sys
+from sys import argv
 import csv
-
-user_endpoint = "https://jsonplaceholder.typicode.com/users/"
 
 if __name__ == "__main__":
     """The script must accept an integer as a
     parameter, which is the employee ID"""
 
-    user_id = sys.argv[1]
+    user_id = argv[1]
     filename = user_id + ".csv"
 
     todos = requests.get('https://jsonplaceholder.typicode.com/todos/')
-    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                        .format(user_id))
+    user = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
 
     name = (user.json().get('username'))
 
