f6495ae1e263f9722ffccaf0467754845bf7f066
Cristiand187 <cristiand187@hotmail.com>
2020-09-14 19:41:24 -0500

    Task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,17 +10,11 @@ if __name__ == "__main__":
     """The script must accept an integer as a
     parameter, which is the employee ID"""
 
-    if len(sys.argv) < 2:
-        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
-        exit(1)
-    user_id = sys.argv[1]
-    filename = user_id + ".json"
+    filename = "todo_all_employees.json"
 
     todos = requests.get('https://jsonplaceholder.typicode.com/todos/')
-    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                        .format(user_id))
+    user = requests.get('https://jsonplaceholder.typicode.com/users/')
 
-    name = (user.json().get('username'))
     with open(filename, "w") as f:
         d = {j.get("id"): [{'task': i.get('title'),
                             'completed': i.get('completed'),
